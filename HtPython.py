dd6cf50c9502d9a672e2b88c5b187ad086543985
Erick <mateguat@hotmail.com>
2020-11-06 13:51:36 -0800

    Hoja de trabajo

--- HtPython.py
+++ HtPython.py
@@ -1,9 +1,12 @@
+subir=1
 def Color(subir):
-     ver=input("dime el color que pienso: ")
-     if ver=="naranja" and subir<3:
-        print("Eres un genio ganaste")
-     else:
-        print("no es ese el color pierdes un intento")
-        subir+=1
-        Color(subir)
-Color()
+        ver=input("dime el color que pienso: ")
+        if ver=="naranja":
+           print("Eres un genio ganaste")
+        elif subir<3:
+                print("no es ese el color pierdes un intento")
+                subir+=1
+                Color(subir) 
+        else:
+            print("pierdes byebye")                    
+Color(subir)
